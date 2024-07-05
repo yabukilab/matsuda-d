d874272312e7f466c6660689b98503ea84145a9e
mahiro0124 <s2242096ea@s.chibakoudai.jp>
2024-07-05 16:06:45 +0900

    20240705

--- htdocs/staff_add_done.php
+++ htdocs/staff_add_done.php
@@ -13,33 +13,21 @@
         <h2>スタッフ追加確認</h2>
             <?php
 
-            try
-            {
+           
 
                 require 'db.php'; 
-print'a';
             $s_name=$_POST['name'];
             $s_pass=$_POST['pass'];
             $sql='insert into 管理者情報管理 (name,pass) values(:s_name,:s_pass)';                    # SQL文
             $prepare = $db->prepare($sql); # 準備
-print'a';
             $prepare->bindValue(':s_name', $s_name, PDO::PARAM_STR); # 埋め込み1
             $prepare->bindValue(':s_pass', $s_pass, PDO::PARAM_STR); # 埋め込み2
-print'a';
             $prepare->execute(); # 実行
-print'a';
             $dbh=null;
-print'a';
             print $s_name;
             print'を追加しました。<br>';
 
-            }catch(Exception$e)
-            {
             
-            print'ただいま障害によりご迷惑をおかけしております。';
-            exit();
-            }
-
             ?>
 
             <a href="s_admin_top.php">戻る</a>
