2295fe714afe83e9dd603201c8b51b120f7c9a87
mahiro0124 <s2242096ea@s.chibakoudai.jp>
2024-07-11 16:29:09 +0900

    20240711

--- htdocs/order_download.php
+++ htdocs/order_download.php
@@ -3,9 +3,14 @@
 <head>
 <meta charset="UTF-8">
 <title> 注文データダウンロード</title>
+<link rel="stylesheet" href="style.css">
 </head>
 <body>
-
+<div class="header">
+    <h1>在庫管理システム</h1>
+	</div>
+    <div class="form">
+        <h2>注文確認</h2>
 <?php
 require_once('common.php');
 ?>
